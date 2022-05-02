310dd1e031f08224890d0ca963cf0acab4decb40
Taejun Moon <junall10022@gmail.com>
2022-05-02 17:15:33 +0900

    Delete Accounts directory

--- webeye/urls.py
+++ webeye/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.basic, name='index'),
-    path('test/', views.classifyvideo, name='test')
+    path('test/', views.classifyvideo, name='test'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout, name='logout'),
 ]
