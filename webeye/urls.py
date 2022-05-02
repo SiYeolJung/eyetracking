1684655cca7ceaeb7d72be1c1b977cf53dbbf4f2
unknown <dragontaek98@naver.com>
2022-05-02 17:36:25 +0900

    feat: video url

--- webeye/urls.py
+++ webeye/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.basic, name='index'),
     path('test/', views.classifyvideo, name='test'),
+    path('video/', views.video, name='video'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
