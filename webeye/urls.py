bd8f3d1894fd425bcf78031ab8c3306607768a39
Taejun Moon <junall10022@gmail.com>
2022-05-02 20:35:34 +0900

    Mypage

--- webeye/urls.py
+++ webeye/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('logout/', views.logout, name='logout'),
+    path('<int:pk>/', views.mypage, name='mypage'),
 ]
