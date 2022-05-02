d2b619b2c43c6f5d2d6167c55460e73baf66fafa
unknown <dragontaek98@naver.com>
2022-05-02 17:24:09 +0900

    modify:account folder

--- eyetracking/urls.py
+++ eyetracking/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('webeye.urls')),
-    path('accounts/', include('accounts.urls')),
 ]
