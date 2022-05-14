1df1ae661e45b29d854a629cbda3ebcc96589eb1
unknown <dragontaek98@naver.com>
2022-05-15 05:31:11 +0900

    feat: additional allowed host

--- eyetracking/settings.py
+++ eyetracking/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','13.209.43.145']
+ALLOWED_HOSTS = ['127.0.0.1','3.34.130.95']
 
 
 # Application definition
