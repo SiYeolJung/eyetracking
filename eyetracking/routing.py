320ce082cbb6c745fc4760775514cacd82c53f36
SiYeolJung <39259086+SiYeolJung@users.noreply.github.com>
2022-05-15 15:22:52 +0900

    style: delete useless annotation

--- eyetracking/routing.py
+++ eyetracking/routing.py
@@ -2,12 +2,8 @@ from email.mime import application
 from django.urls import path 
 
 from .consumers import WSConsumer
-# from .consumers import BoolWS
 
 # Django의 url 과 비슷하다 
 ws_urlpatterns = [
     path('ws/some_url/',WSConsumer.as_asgi())
-    # path('ws/',BoolWS.as_asgi())
-    # path('ws/',BoolWS.as_asgi())
-    
 ]
