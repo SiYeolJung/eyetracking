f24bf2a78a84703e2978d7a960f218b8bd63cb26
unknown <dragontaek98@naver.com>
2022-06-06 17:34:34 +0900

    feat:lecture button design

--- eyetracking/routing.py
+++ eyetracking/routing.py
@@ -5,17 +5,16 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from .consumers import WSConsumer
 
-# application = ProtocolTypeRouter({
-#     'websocket': AuthMiddlewareStack(
-#         #URLRouter(
-#             path('/ws/',WSConsumer.as_asgi())
-#         #)
-#     ),
-# })
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        #URLRouter(
+            path('/ws/',WSConsumer.as_asgi())
+        #)
+    ),
+})
 # -b 0.0.0.0 -p 8443
 
 # Django의 url 과 비슷하다 
-ws_urlpatterns = [
-    path('',WSConsumer.as_asgi()),
-]
-
+# ws_urlpatterns = [
+#     path('',WSConsumer.as_asgi()),
+# ]
